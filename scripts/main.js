f6bdf08d50fdd366fdd49ce41d216015d7facd6c
Abdulaziz <bdalzyzalbrnawy47@gmail.com>
2025-06-12 16:01:46 +0300

    Adding Calculate Functionality

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,33 @@ toggleBtn.addEventListener("click", function() {
     changeMode()
     changeClass(toggleBtn, "", "fa-sun", "fa-moon")
 })
+
+function Calculator() {
+    const grades = document.querySelectorAll('[name=grade]');
+    const hours = document.querySelectorAll('[name=hourse]');
+
+    let totalWeightedPoints = 0;
+    let totalCreditHours = 0;
+    
+    grades.forEach((gradeInput, index) => {
+        const grade = Number(gradeInput.value);
+        const hour = Number(hours[index]?.value); 
+
+        
+
+        if (!isNaN(grade) && !isNaN(hour)) {
+            totalWeightedPoints += grade * hour;
+            totalCreditHours += hour;
+        }
+        
+    });
+
+    const GPA = totalCreditHours > 0 ? (totalWeightedPoints / totalCreditHours).toFixed(2) : "0.00";
+    window.alert(`GPA: ${GPA}`);
+}
+
+
+
+document.querySelector("button").onclick = () => {
+    Calculator();
+}
