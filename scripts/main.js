45145b4c4edcfd3487687a76dd9d78943a8081b2
Abdulaziz <bdalzyzalbrnawy47@gmail.com>
2025-07-03 07:40:04 +0300

    finaly

--- scripts/main.js
+++ scripts/main.js
@@ -96,48 +96,48 @@ function creatingCoursesArray() {
 
 // Used to load elements depending on the local storage
 function loadSavedGrades() {
-    // savedGrades.innerHTML = ``
-    // for (let i = 0; i < localStorage.length; i++) {
-    //     let date = gettingLocalStorageItems(`item-${i+1}`)
-    //     savedGrades.innerHTML += `<div id="SavedItems" class="saved-grade item-${i+1} group">
-    //     <i class="fa-regular fa-file hover-effect text-[22px]"></i>
-    //     <p class="hover-effect">${date[date.length - 1].Date}</p>
-    //     </div>`
-    // }
+    savedGrades.innerHTML = ``
+    for (let i = 0; i < localStorage.length; i++) {
+        let date = gettingLocalStorageItems(`item-${i+1}`)
+        savedGrades.innerHTML += `<div id="SavedItems" class="saved-grade item-${i+1} group">
+        <i class="fa-regular fa-file hover-effect text-[22px]"></i>
+        <p class="hover-effect">${date[date.length - 1].Date}</p>
+        </div>`
+    }
 }
 // First page load
 if (localStorage.length > 0) {
     loadSavedGrades()
 }
 
-const mySaveButton = document.querySelector("[type=button]");
+const mySaveButton = document.querySelector(".save-btn");
 mySaveButton.addEventListener("click", () => {
     SaveToLocalStorage();
 });
 
 // Used to read local storage objects
 function gettingLocalStorageItems(element) {
-    // let unFormatedCourse = localStorage.getItem(element) 
-    // let formatedCourse = JSON.parse('[' + unFormatedCourse.replace(/}{/g, '},{') + ']');
-    // return formatedCourse
+    let unFormatedCourse = localStorage.getItem(element) 
+    let formatedCourse = JSON.parse('[' + unFormatedCourse.replace(/}{/g, '},{') + ']');
+    return formatedCourse
 }
 
 // Used to write data from the local sotrage object
 function LoadFromLocalStorage(key) {
-    // let coursesArray = gettingLocalStorageItems(key)
-    // for (let i = 0; i < courses.length; i++) {
-    //     courses[i].value = ""
-    //     grades[i].value = ""
-    //     hours[i].value = ""
-    // }
-    // for (let i = 0; i < localStorage.length; i++) {
-    //     for (let j = 0; j < coursesArray.length - 1; j++) {
-    //         let {Name, Grade, Hours} = coursesArray[j]
-    //         Name !== null ? courses[j].value = Name : "";
-    //         if (Grade !== null) grades[j].value = Grade;
-    //         if (Hours !== null) hours[j].value = Hours;
-    //     }
-    // }
+    let coursesArray = gettingLocalStorageItems(key)
+    for (let i = 0; i < courses.length; i++) {
+        courses[i].value = ""
+        grades[i].value = ""
+        hours[i].value = ""
+    }
+    for (let i = 0; i < localStorage.length; i++) {
+        for (let j = 0; j < coursesArray.length - 1; j++) {
+            let {Name, Grade, Hours} = coursesArray[j]
+            Name !== null ? courses[j].value = Name : "";
+            if (Grade !== null) grades[j].value = Grade;
+            if (Hours !== null) hours[j].value = Hours;
+        }
+    }
 }
 
 const savedItmesButton = document.getElementById("SavedItems");
@@ -167,15 +167,15 @@ function add_row() {
             <div class="grow">
                 <p>Grade</p>
                 <select name="grade" class="w-full custom-input" required>
-                    <option value="4">A+</option>
-                    <option value="3.75">A</option>
-                    <option value="3.5">B+</option>
-                    <option value="3">B</option>
-                    <option value="2.5">C+</option>
-                    <option value="2">C</option>
-                    <option value="1.5">D+</option>
-                    <option value="1">D</option>
-                    <option value="0">F</option>
+                    <option value="4">A+ (4)</option>
+                    <option value="3.75">A (3.75)</option>
+                    <option value="3.5">B+ (3.5)</option>
+                    <option value="3">B (3)</option>
+                    <option value="2.5">C+ (2.5)</option>
+                    <option value="2">C (2)</option>
+                    <option value="1.5">D+ (1.5)</option>
+                    <option value="1">D (1)</option>
+                    <option value="0">F (0)</option>
                 </select>
             </div>
             <div class="trash">
@@ -211,11 +211,11 @@ checkBox.addEventListener("click", () => {
             <div class="flex flex-row gap-4 cumliativeFeilds" >
                 <div>
                     <p>Completed Credit Hours</p>
-                    <input type="number" class="custom-input" id="completed-credit-hours" min="1">
+                    <input type="number" class="custom-input" id="completed-credit-hours" min="1" required>
                 </div>
                 <div>
                     <p>Cumilative GPA</p>
-                    <input type="number" class="custom-input" id="overall-gpa" min="0" step="0.01">
+                    <input type="number" class="custom-input" id="overall-gpa" min="0" step="0.01" required>
                 </div>
             </div>
             `;
