775b6b50c4f71d9f9431f6f5f2c3c74d540a771c
Faisal-Waleed-Dawoud <faisal.dawoud417@gmail.com>
2025-07-07 15:29:26 +0300

    Final-edits

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,10 @@ let creditHours = 0
 let overallGPA = 0
 let totalPoints = 0
 let savedGrades = document.getElementById("items-container")
-const grades = document.querySelectorAll('.grow [name=grade]');
-const hours = document.querySelectorAll('.grow [name=hours]');
-const courses = document.querySelectorAll('.grow [type=text]');
+const grades = document.querySelectorAll('.grade');
+const hours = document.querySelectorAll('.hours');
+const courses = document.querySelectorAll('.course');
 let courseNumber = window.localStorage.length
-const checkBox = document.querySelector("input[type=checkbox]");
-const cumliativeContainer = document.querySelector("#cumliative");
 
 function changeMode() {
     document.documentElement.classList.toggle("dark")
@@ -23,12 +21,20 @@ function changeClass(parent, element, desiredClass, oldClass) {
 }
 
 
-
-function calculateTotalPoints(currentHourse, totalWeightedPoints) {
+function calculateTotalPoints(currentHours, totalWeightedPoints) {
     creditHours = +document.querySelector("#completed-credit-hours").value;
     overallGPA = +document.querySelector("#overall-gpa").value;
-    return ((overallGPA * creditHours + totalWeightedPoints) / (creditHours + currentHourse)).toFixed(2)
+
+    
+    if (creditHours === 0 || overallGPA === 0) {
+        return undefined
+    }
+    
+    return ((overallGPA * creditHours + totalWeightedPoints) / (creditHours + currentHours)).toFixed(2)
 }
+
+cumilativeGPA.addEventListener("change", Calculator)
+completedCreditHours.addEventListener("change", Calculator)
 
 
 toggleBtn.addEventListener("click", function() {
@@ -55,22 +61,19 @@ function Calculator() {
             gradesSum += grade;
         }
     });
-
- 
-    let totalGPA;
-    if (checkBox.checked)
-    {
-        totalGPA = calculateTotalPoints(totalCreditHours, totalWeightedPoints);
-    }
+    
+    
+    
+    let totalGPA = calculateTotalPoints(totalCreditHours, totalWeightedPoints);
     const termGPA = totalCreditHours > 0 ? (totalWeightedPoints / totalCreditHours).toFixed(2) : "0.00";
-    ShowMessage(termGPA, totalGPA.toFixed(2));
+    ShowMessage(termGPA, totalGPA);
 }
 
 function ShowMessage(termGPA, totalGPA) {
     let cumlativeResult = document.getElementById("cumlative-result")
     let termResult = document.getElementById("semster-result")
-    termResult.innerText = +termGPA
-    cumlativeResult.innerText = totalGPA
+    termResult.innerHTML = +termGPA
+    cumlativeResult.innerHTML = totalGPA || termGPA
 }
 
 courses.forEach((course) => {
@@ -90,24 +93,27 @@ function SaveToLocalStorage() {
 
 // Stores the objects in the local storage
 function creatingCoursesArray() {
-    const grades = document.querySelectorAll('.grow [name=grade]');
-    const courses = document.querySelectorAll('.grow [type=text]');
-    const hours = document.querySelectorAll('.grow [name=hours]');
+    const grades = document.querySelectorAll('.grade');
+    const courses = document.querySelectorAll('.course');
+    const hours = document.querySelectorAll('.hours');
+    creditHours = +document.querySelector("#completed-credit-hours").value;
+    overallGPA = +document.querySelector("#overall-gpa").value;
     let savingDate = new Date
     let day = savingDate.getDate()
     let month = savingDate.getMonth() + 1
     let year = savingDate.getFullYear()
     let coursesArray = []
     grades.forEach((e, i) => {
-            coursesArray.push(JSON.stringify({"Name" : courses[i].value.trim(), "Grade" : e.value, "Hours": hours[i].value}))
-        })
-        coursesArray.push(JSON.stringify({"Date": `${day}-${month}-${year}`}))
-        courseNumber += 1
-        return [coursesArray, courseNumber]
-    }
-    
-    
-    // Used to load elements depending on the local storage
+        coursesArray.push(JSON.stringify({"Name" : courses[i].value.trim(), "Grade" : e.value, "Hours": hours[i].value}))
+    })
+    coursesArray.push(JSON.stringify({"CumlitaiveGPA": overallGPA}))
+    coursesArray.push(JSON.stringify({"CompletedCreditHours": creditHours}))
+    coursesArray.push(JSON.stringify({"Date": `${day}-${month}-${year}`}))
+    courseNumber += 1
+    return [coursesArray, courseNumber]
+}
+
+// Used to load elements depending on the local storage
 function loadSavedGrades() {
     savedGrades.innerHTML = ""
     for (let i = 0; i < localStorage.length; i++) {
@@ -138,19 +144,25 @@ function gettingLocalStorageItems(element) {
 // Used to write data from the local sotrage object
 function LoadFromLocalStorage(key) {
     let coursesArray = gettingLocalStorageItems(key)
-    const courses = document.querySelectorAll('.grow [type=text]');
-    const hours = document.querySelectorAll('.grow [name=hours]');
-    const grades = document.querySelectorAll('.grow [name=grade]');
+    const courses = document.querySelectorAll('.course');
+    const hours = document.querySelectorAll('.hours');
+    const grades = document.querySelectorAll('.grade');
+    let {CumlitaiveGPA} = coursesArray[coursesArray.length - 3]
+    let {CompletedCreditHours} = coursesArray[coursesArray.length - 2]
+
+    cumilativeGPA.value = CumlitaiveGPA
+    completedCreditHours.value = CompletedCreditHours
+
     for (let i = 0; i < courses.length; i++) {
         courses[i].value = ""
         grades[i].value = ""
         hours[i].value = ""
     }
-        for (let j = 0; j < coursesArray.length - 1; j++) {
-            const grades = document.querySelectorAll('.grow [name=grade]');
-            const courses = document.querySelectorAll('.grow [type=text]');
-            const hours = document.querySelectorAll('.grow [name=hours]');
-            if (grades.length < coursesArray.length -1) {
+        for (let j = 0; j < coursesArray.length - 3; j++) {
+            const grades = document.querySelectorAll('.grade');
+            const courses = document.querySelectorAll('.course');
+            const hours = document.querySelectorAll('.hours');
+            if (grades.length < coursesArray.length -3) {
                 createRow()
             }
             let {Name, Grade, Hours} = coursesArray[j]
@@ -158,6 +170,7 @@ function LoadFromLocalStorage(key) {
             grades[j].value = Grade;
             hours[j].value = Hours;
         }
+    Calculator()
 }
 
 
@@ -175,17 +188,17 @@ function createRow() {
         row.className = "flex gap-4 flex-row";
         row.style = `margin-top: 1rem`;
         row.innerHTML = `
-            <div class="grow">
-                <p>Course Name</p>
-                <input type="text" class="custom-input">
+            <div>
+                <p class="text-sm">اسم المادة</p>
+                <input type="text" class="custom-input course">
             </div>
-            <div class="grow">
-                <p>Credit Hours</p>
-                <input type="number" name="hours" min="1" class="w-full custom-input" required>
+            <div>
+                <p class="text-sm text-nowrap">الساعات الدراسية</p>
+                <input type="number" name="hours" min="1" class="w-full custom-input hours" required>
             </div>
-            <div class="grow">
-                <p>Grade</p>
-                <select name="grade" class="w-full custom-input" required>
+            <div>
+                <p class="text-sm">الدرجة</p>
+                <select name="grade" class="w-full custom-input grade" required>
                     <option value="4">A+ (4)</option>
                     <option value="3.75">A (3.75)</option>
                     <option value="3.5">B+ (3.5)</option>
@@ -202,8 +215,8 @@ function createRow() {
             </div>
         `;
 
-        row.querySelector('[name=grade]').addEventListener('change', Calculator);
-        row.querySelector('[name=hours]').addEventListener('change', Calculator);
+        row.querySelector('.grade').addEventListener('change', Calculator);
+        row.querySelector('.hours').addEventListener('change', Calculator);
 
         container.appendChild(row);
 
@@ -222,27 +235,27 @@ savedGrades.addEventListener("click", (e) => {
 })
 
 
-checkBox.addEventListener("click", () => {
+// checkBox.addEventListener("click", () => {
     
-    const feilds = document.createElement("div");
-    if (checkBox.checked) {
-        feilds.innerHTML = `
-            <div class="flex flex-row gap-4 cumliativeFeilds" >
-                <div>
-                    <p>Completed Credit Hours</p>
-                    <input type="number" class="custom-input" id="completed-credit-hours" min="1" required>
-                </div>
-                <div>
-                    <p>Cumilative GPA</p>
-                    <input type="number" class="custom-input" id="overall-gpa" min="0" step="0.01" required>
-                </div>
-            </div>
-            `;
-            cumliativeContainer.appendChild(feilds);
-    }
+//     const feilds = document.createElement("div");
+//     if (checkBox.checked) {
+//         feilds.innerHTML = `
+//             <div class="flex flex-row gap-4 cumliativeFeilds" >
+//                 <div>
+//                     <p>Completed Credit Hours</p>
+//                     <input type="number" class="custom-input" id="completed-credit-hours" min="1" required>
+//                 </div>
+//                 <div>
+//                     <p>Cumilative GPA</p>
+//                     <input type="number" class="custom-input" id="overall-gpa" min="0" step="0.01" required>
+//                 </div>
+//             </div>
+//             `;
+//             cumliativeContainer.appendChild(feilds);
+//     }
 
-    else {
-        const exsitedFeilds = document.querySelector(".cumliativeFeilds");
-        exsitedFeilds.remove();
-    }
-})
+//     else {
+//         const exsitedFeilds = document.querySelector(".cumliativeFeilds");
+//         exsitedFeilds.remove();
+//     }
+// })
